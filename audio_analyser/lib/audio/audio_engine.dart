9e18b73c563d193537d115f58fd17cb1f486b222
Barabas Raffai <barabas.raffai@gmail.com>
2024-10-17 17:01:48 +0100

    format

--- audio_analyser/lib/audio/audio_engine.dart
+++ audio_analyser/lib/audio/audio_engine.dart
@@ -4,7 +4,6 @@ import 'package:audio_analyser/audio/meters_state.dart';
 import 'package:audio_analyser/backend/proto/generated/audio_analyser.pbgrpc.dart'
     as grpc;
 
-
 typedef AudioBuffer = List<double>;
 
 abstract class AudioEngine {
@@ -24,10 +23,10 @@ class GrpcAudioEngine implements AudioEngine {
       client.getAudioStream(grpc.Void()).map((event) => event.samples);
 
   @override
-  Stream<MetersState> get meters =>
-      client.getMeterStream(grpc.Void()).map((event) => MetersState(rms: event.rms));
+  Stream<MetersState> get meters => client
+      .getMeterStream(grpc.Void())
+      .map((event) => MetersState(rms: event.rms));
 
   @override
   void dispose() {}
 }
-
